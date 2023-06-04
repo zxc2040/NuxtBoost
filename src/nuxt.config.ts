697b8faa919e50189371b8622041d7a2afe62bf4
Teerachai Laothong <tlaothong@gmail.com>
2023-06-04 09:42:27 +0700

    Init Flowbite with Tailwind. Also pinia and vueuse

---
+++ src/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-icon',
+  ],
+  tailwindcss: [
+    '~/assets/css/appbase.css',
+  ],
+})
