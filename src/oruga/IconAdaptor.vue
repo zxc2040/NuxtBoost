b58fc5c8bea7aa693d50c30287838fa822c857d5
Teerachai Laothong <tlaothong@gmail.com>
2023-06-05 09:06:32 +0700

    Added IconAdaptor and use in Oruga.

---
+++ src/oruga/IconAdaptor.vue
@@ -0,0 +1,17 @@
+<template>
+  <Icon :name="IconName"></Icon>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ icon: Array<String> }>();
+
+const IconName = computed(() => {
+  const [ _pack, _name ] = props.icon;
+
+  if (_pack == 'mdi') {
+    return `mdi:${_name}`;
+  }
+
+  return `${_pack}:${_name}`;
+});
+</script>
