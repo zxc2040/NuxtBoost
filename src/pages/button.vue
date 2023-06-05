af004f74006da140d5838056f8e9505f3df9f25c
zxc2040 <107449746+zxc2040@users.noreply.github.com>
2023-06-05 20:26:42 +0700

    commit button

---
+++ src/pages/button.vue
@@ -0,0 +1,66 @@
+<template>
+    <o-button
+        rootClass="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        Primary
+    </o-button>
+
+    <o-button
+    variant="danger"
+        rootClass="focus:outline-none text-white  focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">
+        Danger
+    </o-button>
+
+    <o-button
+        rootClass="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Primary
+    </o-button>
+
+    <o-button
+        rootClass="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 mr-2 text-center">
+        Disabled button
+    </o-button>
+
+    <o-button
+        rootClass="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+        Default
+    </o-button>
+
+    <o-button
+        rootClass="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+        elementsWrapperClass="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        Purple to blue
+    </o-button>
+
+    <o-button
+        rootClass="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+        iconClass="mr-1" icon-left="facebook">
+        Sign in with Facebook
+    </o-button>
+
+    <o-button
+        rootClass="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+        iconClass="mr-1" icon-left="ethereum" expandedClass="expanded">
+        Pay with Ethereum
+    </o-button>
+
+    <o-button
+        rootClass="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
+        <o-icon rootClass="animate-spin" icon="sync"> </o-icon>
+        Loading...
+    </o-button>
+
+
+    <o-switch rootClass="relative inline-flex items-center mb-4 cursor-pointer mr-5"
+        checkClass="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+        inputClass="sr-only peer"
+        labelClass="relative inline-flex items-center ml-3  text-sm font-medium text-gray-900 dark:text-gray-300">
+        Default
+    </o-switch>
+
+
+</template>
+<style>
+.o-btn--danger  {
+    @apply bg-red-700 hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 
+}
+</style>
