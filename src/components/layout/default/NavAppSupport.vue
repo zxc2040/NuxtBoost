075634d263f840b667e59839f523989736474b65
Teerachai Laothong <tlaothong@gmail.com>
2023-06-06 01:05:16 +0700

    Reuse menu item in support bar.

--- src/components/layout/default/NavAppSupport.vue
+++ src/components/layout/default/NavAppSupport.vue
@@ -1,31 +1,13 @@
 <template>
   <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
     <li>
-      <a href="#"
-        class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-        <Icon aria-hidden="true"
-          class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          name="mdi:help-box-multiple"></Icon>
-        <span class="ml-3">Docs</span>
-      </a>
+      <MenuItem icon-name="mdi:help-box-multiple">Docs</MenuItem>
     </li>
     <li>
-      <a href="#"
-        class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-        <Icon aria-hidden="true"
-          class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          name="mdi:puzzle"></Icon>
-        <span class="ml-3">Components</span>
-      </a>
+      <MenuItem icon-name="mdi:puzzle">Components</MenuItem>
     </li>
     <li>
-      <a href="#"
-        class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-        <Icon aria-hidden="true"
-          class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          name="mdi:face-agent"></Icon>
-        <span class="ml-3">Help</span>
-      </a>
+      <MenuItem icon-name="mdi:face-agent">Help & Support</MenuItem>
     </li>
   </ul>
 </template>
